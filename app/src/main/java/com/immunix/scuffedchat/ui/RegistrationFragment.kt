0377323ae979a61b6a17500182643d7bbc2f4a80
Immunix <mirceagighileanu@yahoo.com>
2021-01-22 23:29:37 +0100

    Initial commit

---
+++ app/src/main/java/com/immunix/scuffedchat/ui/RegistrationFragment.kt
@@ -0,0 +1,8 @@
+package com.immunix.scuffedchat.ui
+
+import androidx.fragment.app.Fragment
+import com.immunix.scuffedchat.R
+
+class RegistrationFragment : Fragment(R.layout.fragment_registration) {
+
+}
